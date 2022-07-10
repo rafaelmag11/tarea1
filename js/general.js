d8fd9d7d59f8d28e4671b0496c5bd4d1f61fa9da
rafaelmag11 <109003901+rafaelmag11@users.noreply.github.com>
2022-07-09 19:06:27 -0500

    Create general.js

---
+++ js/general.js
@@ -0,0 +1,76 @@
+﻿var General = {
+
+    consultaArray: function(parametros, opcionConsulta, funcion){
+
+		$.ajax({
+
+			data:  parametros,
+
+	        url:   'controller/consultas.php?opcion=' + opcionConsulta,
+
+			dataType: 'json',
+
+			cache: false,
+
+	        type:  'post',
+
+			beforeSend: function () {
+
+	            
+
+	        },
+
+	        success:  function(datos){
+
+				funcion(datos);
+
+			},
+
+			error: function(xhr, status, error){
+
+				$('#modalCarga').modal('hide');
+				swal("Error", "Falló la conexión con el servidor, opcion:" + opcionConsulta, "error");
+
+			}
+
+	    });
+
+    },
+
+	consultaArray2: function(parametros, opcionConsulta, funcion, funcionError){
+
+		$.ajax({
+
+			data:  parametros,
+
+	        url:   'controller/consultas.php?opcion=' + opcionConsulta,
+
+			dataType: 'json',
+
+			cache: false,
+
+	        type:  'post',
+
+			beforeSend: function () {
+
+	            
+
+	        },
+
+	        success:  function(datos){
+
+				funcion(datos);
+
+			},
+
+			error: function(){
+
+				funcionError();
+
+			}
+
+	    });
+
+    }
+
+};
