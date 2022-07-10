a77a22d4ffbbf36127e9abcca97fac4cf5561ae8
rafaelmag11 <109003901+rafaelmag11@users.noreply.github.com>
2022-07-09 19:02:51 -0500

    Create menu.php

---
+++ parametros/menu.php
@@ -0,0 +1,48 @@
+<?php
+
+$menu = array(
+
+			array(1,'Tickets Reportados','iconoAlert.svg','inicio',array(),0),
+
+			array(2,'Nuevo Ticket','iconoPlus.svg','nuevo-ticket',array(),0),
+
+			array(3,'Tickets por Atender','iconoNotifications.svg','monitor',array(),1)
+
+			/*array(2,'Reportes','fa-caret-square-o-down','reportes',
+
+				array(
+
+					array(1,'reporte1','reporte X')
+
+				)
+
+			)*/
+
+		);
+
+$menuCat = array(
+
+			array(4,'Usuarios','iconoUser.svg','cat-usuarios',array(),1),
+
+			array(5,'Proyectos','iconoProyectos.svg','cat-aplicaciones',array(),1),
+
+			/*array(6,'Empresas','icon-empresas','cat-empresas',array(),1),*/
+
+			array(6,'Áreas','iconoSecciones.svg','cat-areas',array(),1),
+
+			/*array(8,'Permisos','icon-permisos','cat-permisos',array(),1),*/
+
+			array(7,'Correos','iconoEmail.svg','cat-correos',array(),1)
+		);
+
+$menuOrd = array(
+
+			array(8,'Ordenes de trabajo','icon-ordenes','ordenes-trabajo',array(),1)
+		);
+
+$menuPrincipal = array(
+
+			array(9,'Administrador','icon-usuarios','admin',array(),1)
+		);
+
+?>
